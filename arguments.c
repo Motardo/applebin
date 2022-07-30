ac7ab5697d9a2c2e071f520d4dfb2ce5ffc70739
Nathan Wilson <nswilson@gmail.com>
2022-07-30 13:56:14 -0400

    Add help text

--- arguments.c
+++ arguments.c
@@ -8,11 +8,22 @@ static void printHelp(void);
 static void printVersion(void);
 
 static void printHelp() {
-  printf("Help\n");
+  char* help =
+"Usage: applebin [OPTION]... FILE [FILE]\n"
+"Convert an AppleSingle or AppleDouble FILE to MacBinary format.\n"
+"\n"
+"Mandatory arguments to long options are mandatory for short options too.\n"
+"  -o, --out-file=FILE   output converted MacBinary file to FILE\n"
+"  -l, --list-only       display a summary of the file(s) but do not convert\n"
+"  -d, --debug           display the type, length, and offset of header entries\n"
+"  -h, --help            display this help and exit\n"
+"  -v, --version         output version information and exit\n"
+;
+  printf("%s\n", help);
   exit(EXIT_SUCCESS);
 }
 static void printVersion() {
-  printf("Version\n");
+  printf("Version 1.0\n");
   exit(EXIT_SUCCESS);
 }
 
@@ -26,21 +37,21 @@ Arguments* commandArgs(int argc, char **argv) {
     static struct option long_options[] =
       {
        {"help",       no_argument,       0, 'h'},
-       {"version",    no_argument,       0, 'V'},
-       {"verbose",    no_argument,       0, 'v'},
-       {"no-create",  no_argument,       0, 'n'},
+       {"version",    no_argument,       0, 'v'},
+       {"debug",      no_argument,       0, 'd'},
+       {"list-only",  no_argument,       0, 'l'},
        {"out-file",   required_argument, 0, 'o'},
        {0,            0,                 0,  0 }
       };
 
     int option_index = 0;
-    int c = getopt_long(argc, argv, "hVvno:",
+    int c = getopt_long(argc, argv, "hvdlo:",
                     long_options, &option_index);
     if (c == -1) break;
     else if (c == 'h') printHelp();
-    else if (c == 'V') printVersion();
-    else if (c == 'v') args.verbose = 1;
-    else if (c == 'n') args.noCreate = 1;
+    else if (c == 'v') printVersion();
+    else if (c == 'd') args.verbose = 1;
+    else if (c == 'l') args.noCreate = 1;
     else if (c == 'o') args.outFileName = strdup(optarg);
     else {
       fprintf(stderr, "%s: unknown option '%c'\n", argv[0], c);
